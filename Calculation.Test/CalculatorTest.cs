c9b5e5da84739429645ee2c5d498294010a4fd27
alex <839928622@qq.com>
2020-01-19 13:18:27 +0800

    1.创建项目 2.创建测试的类库，在测试类库中添加对待测试项目的引用，安装xUnit包。

---
+++ Calculation.Test/CalculatorTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Calculation.Test
+{
+  public  class CalculatorTest
+    {
+        [Fact]
+        public void TestAdd()
+        {
+            Assert.True(true);
+        }
+    }
+}
