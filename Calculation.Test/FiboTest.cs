7df78b73fac230e9dc7b5be7b24a2f6c5be0efcd
alex <839928622@qq.com>
2020-01-19 17:35:34 +0800

    '集合之间相等，包含，不包含的关系'

---
+++ Calculation.Test/FiboTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Calculation.Test
+{
+    public class FiboTest
+    {
+        [Fact]
+        public void FiboNumbersNotContainsZero()
+        {
+            var fibo = new Fibo();
+            Assert.All(fibo.fiboNumbers, n => Assert.NotEqual(0,n));//对集合遍历，对某一个元素进行测试
+        }
+
+        [Fact] //斐波那契数列必须包含13
+        public void FiboMustContains13()
+        {
+            var fibo = new Fibo();
+            Assert.Contains(13, fibo.fiboNumbers);//后者包含前者
+        }
+
+        [Fact]
+        public void FiboNotContains4()
+        {
+            var fibo = new Fibo();
+             Assert.DoesNotContain(4,fibo.fiboNumbers);//后者不包含前者
+        }
+
+        [Fact]
+        public void FiboCollectionTest()
+        {
+            var fiboList = new List<int>() { 1, 1, 2, 3, 5, 8, 13, 21 };
+            var fibo = new Fibo();
+            Assert.Equal(fiboList,fibo.fiboNumbers);//两个集合是否相等
+
+        }
+    }
+}
