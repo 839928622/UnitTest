b232918950b56c519fbd864c89d71ef83ec834e2
蒋文科 <839928622@qq.com>
2020-01-19 22:45:09 +0800

    对数值 区间的测试

---
+++ Calculation.Test/CustomerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Calculation.Test
+{
+  public  class CustomerTests
+    {
+        [Fact]
+        public void CheckNameNotEmpty()
+        {
+            var customer = new Customer();
+            Assert.NotNull(customer.Name);
+            Assert.False(string.IsNullOrWhiteSpace(customer.Name));//验证性命不能为空
+        }
+
+        [Fact]
+        public void CheckAgeForDiscount()
+        {
+            var  customer = new  Customer();
+            Assert.InRange(customer.Age,25,40);//数值是否在该范围 年龄默认值是35
+            Assert.InRange(customer.Age, 35, 35);// 这个是大于等于且小于等于的关系
+        }
+    }
+}
---
+++ Calculation/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculation
+{
+   public class Customer
+   {
+       public string Name => "Alex";
+       public int Age  => 35;
+    }
+}
