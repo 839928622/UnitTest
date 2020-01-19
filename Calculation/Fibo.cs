9d0b2ac560ce16dd7e53cd9a9edb41577cc5331d
alex <839928622@qq.com>
2020-01-19 17:37:26 +0800

    '集合之间的比较'

---
+++ Calculation/Fibo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace Calculation
+{
+   public class Fibo
+    {
+        public List<int> fiboNumbers { get; set; } = new List<int>() { 1, 1, 2, 3, 5, 8, 13, 21 };
+        
+    }
+}
