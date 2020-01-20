052a3046959a4b62390a3e43c3c235708ffbd663
蒋文科 <839928622@qq.com>
2020-01-20 16:44:14 +0800

    检测返回的对象的类型

--- Calculation.Test/CustomerTests.cs
+++ Calculation.Test/CustomerTests.cs
@@ -30,7 +30,21 @@ namespace Calculation.Test
             Assert.Throws<ArgumentNullException>(() => customer.GetCustomerAddressNo(null));//判断是否会抛出异常
 
             var exception = Assert.Throws<ArgumentNullException>(() => customer.GetCustomerAddressNo(null));//可以获取异常里的消息
-             Assert.Equal("错误消息", exception.Message);   
+             Assert.Equal("customerName", exception.ParamName);   
+        }
+
+        [Fact]
+        public void OverrideMethodCheck()
+        {
+            var customer = new Customer();
+            Assert.Equal(1,customer.GetCustomerAddressNo("你好"));//说明实例化父类，执行的还是父类的方法。
+        }
+
+        [Fact]
+        public void CustomerReturnTypeCheckWhenOrderCountGreaterThan100()
+        {
+            Assert.IsType<LoyalCustomer>(CustomerFactory.CreateCustomerInstance(122));//检验对象（object）的类型
+           // Assert.IsType(typeof(LoyalCustomer),CustomerFactory.CreateCustomerInstance(102));//这里不建议使用typeof去检查 对象的类型
         }
     }
 }
--- Calculation/Customer.cs
+++ Calculation/Customer.cs
@@ -9,14 +9,38 @@ namespace Calculation
        public string Name => "Alex";
        public int Age  => 35;
 
-       public int GetCustomerAddressNo(string customerName)
+       public virtual int GetCustomerAddressNo(string customerName)
        {
            if (string.IsNullOrWhiteSpace(customerName))
            {
-               throw new ArgumentNullException();
+               throw new ArgumentNullException(nameof(customerName));
            }
 
            return 1;
        }
     }
+
+
+   public class LoyalCustomer:Customer
+   {
+       public LoyalCustomer()
+       {
+           Discount = 10;
+       }
+       public int Discount { get; set; }
+
+       public override int GetCustomerAddressNo(string customerName)
+       {
+           return 102;
+       }
+   }
+
+
+   public static class CustomerFactory
+   {
+       public static Customer CreateCustomerInstance(int orderCount)
+       {
+           return orderCount > 100 ? new LoyalCustomer() : new Customer();
+       }
+   }
 }
