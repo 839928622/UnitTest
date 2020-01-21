9211f1113e64c2f3b5992fe2abc674f4506129c2
蒋文科 <839928622@qq.com>
2020-01-21 21:10:11 +0800

    1.如果我们没有在每一个测试的类上放置Collecftion特性的话，Xunit会对每一个测试类里的具体的测试方法并行执行，也就是多线程了，每个类一个线程。但是当在几个测试类中放置Collection后，Xunit会认为放置了这几个特性的类是一类测试，会创建一个线程去测试，然后不同测试类里的方法按什么顺序被执行的话：他们会被创建的这一个线程以序列管理器依次执行。 在之前写的代码中，我们只给了CustoemerTests和CustomerDetailTest进行Collection特性的放置，其他测试类没有放置，那么情况就变了，放置了的类被xunit创建一个线程去执行；没有放置的测试类仍然由另一个线程去执行，每一个类一个线程。如果想要更高效地执行测试用例，可以使用collection对测试进行分类，然后xunit并行执行测试用例。 2.放置了Collection的类，以什么顺序去执行测试呢？ 这里就需要一个顺序管理器，我们可以手动添加类，叫TestCollectionOrder，并实现ITestCollectionOrderer接口，具体在TestCollectionOrder中查看。 3.注意：ClassFixture和CollectionFixture实现了共享上下文，如果某个类有副作用，会影响最终的测试结果。共享上下文，就是共享某个类，但是该类里面的方法耦合度高，互相依赖，也就是副作用。

---
+++ Calculation.Test/TestCollectionOrder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Calculation.Test
+{
+   public class TestCollectionOrder:ITestCollectionOrderer
+    {
+        public IEnumerable<ITestCollection> OrderTestCollections(IEnumerable<ITestCollection> testCollections)
+        {
+            return testCollections.OrderBy(x => x.DisplayName);//这里可以对放置了Collection特性的测试类，按什么顺序去执行测试
+            //问题：我们可以定义多个类，然后实现ITestCollectionOrderer接口，但是Xunit如何知道对于多个放置了xunit的测试类，怎么作用该排序呢？
+            //回答：需要在AssemblyInfo.cs文件里指定，这个文件如果是vs的话，会自动创建，如果没有的话可能需要手动创建。需要Using Xunit，
+            // 然后在底部添加这样一条代码：[assembly:TestCollectionOrderer("第一个参数","第二个参数")]
+            //其中，第一个参数是我们当前类所在的命名空间，当前类就是TestCollectionOrder，它的命名空间是Calculation.Test，需要把命名空间和类结合起来，
+            //那么最终的第一个参数就是：Calculation.Test.TestCollectionOrder,
+            //第二个参数，是项目的命名空间，显然，就是Calculation.Test。
+        }
+    }
+}
